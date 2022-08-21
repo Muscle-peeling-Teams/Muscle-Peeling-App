94f97be5d290dc07dd36b82500a8ec89f846e035
kazua33 <21cm0133@jec.ac.jp>
2022-08-21 17:53:17 +0900

    筋トレメニューを実行できるところまで

--- MotionManager/MotionManager.swift
+++ MotionManager/MotionManager.swift
@@ -5,8 +5,8 @@
 //  Created by cmStudent on 2022/07/22.
 //
 
-import Foundation
 import CoreMotion
+import SwiftUI
 
 final class MotionManager: ObservableObject{
     // staticでインスタンスを保持しておく
@@ -17,9 +17,19 @@ final class MotionManager: ObservableObject{
     
     private let queue = OperationQueue()
     
+    @Published var backColor = Color.white
+    
+    @Published var sensorSucess = false
+    
+    @Published var systemImage = "xmark"
+    
     @Published var x = 0.00
     @Published var y = 0.00
     @Published var z = 0.00
+    
+    @Published var countDown = 5
+    
+    var timer: Timer?
     
     // シングルトンにするためにinitを潰す
     private init() {}
@@ -41,9 +51,41 @@ final class MotionManager: ObservableObject{
                     self.y = validData.gravity.y
                     self.z = validData.gravity.z
                 }
+            
                 print("x: \(self.x)")
                 print("y: \(self.y)")
                 print("z: \(self.z)")
+            }
+        }
+    }
+    
+    func startTimer() {
+        sensorSucess = true
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+            self.countDown -= 1
+            print("\(self.countDown)")
+            if self.countDown <= 0 {
+                self.timer?.invalidate()
+                // TODO:複数対応できるように変更
+                self.plank()
+            }
+        }
+    }
+    
+    func plank() {
+        startQueuedUpdates()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            self.systemImage = self.sensorSucess ? "circle":"xmark"
+            self.backColor = self.sensorSucess ? Color.white : Color.red
+            // TODO: 他の分岐方法を考える
+            if self.x <= -0.08 && self.x >= -0.12 {
+                if self.y <= 1.00 && self.y >= 0.98 || self.y >= -1.00 && self.y <= -0.98 {
+                    self.sensorSucess = true
+                } else {
+                    self.sensorSucess = false
+                }
+            } else {
+                self.sensorSucess = false
             }
         }
     }
---
+++ MotionManager/PlankView.swift
@@ -0,0 +1,42 @@
+//
+//  PlankView.swift
+//  MotionManager
+//
+//  Created by 稗田一亜 on 2022/08/21.
+//
+
+import SwiftUI
+
+struct PlankView: View {
+    @ObservedObject var motionManager : MotionManager  = .shared
+    var body: some View {
+        ZStack {
+            motionManager.backColor
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image(systemName: motionManager.systemImage)
+                    .resizable()
+                    .frame(width: 70, height: 70)
+                Spacer()
+                if (motionManager.sensorSucess) {
+                    Text("\(motionManager.countDown)")
+                } else {
+                    Button("START"){
+                        motionManager.startTimer()
+                    }
+                    .font(.largeTitle)
+                }
+                Spacer()
+                
+            }
+        }
+        
+    }
+}
+
+struct PlankView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlankView()
+    }
+}
