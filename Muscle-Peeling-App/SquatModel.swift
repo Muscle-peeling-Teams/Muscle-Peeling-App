6b5d367f50751ccd1d17e98087174382ece78b64
45Watanabe <94812163+45Watanabe@users.noreply.github.com>
2022-08-24 10:21:12 +0900

    Squat用フォルダ及びファイルの追加

---
+++ Muscle-Peeling-App/SquatModel.swift
@@ -0,0 +1,8 @@
+//
+//  SquatModel.swift
+//  Muscle-Peeling-App
+//
+//  Created by 渡辺幹 on 2022/08/24.
+//
+
+import Foundation
---
+++ Muscle-Peeling-App/SquatView.swift
@@ -0,0 +1,20 @@
+//
+//  SquatView.swift
+//  Muscle-Peeling-App
+//
+//  Created by 渡辺幹 on 2022/08/24.
+//
+
+import SwiftUI
+
+struct SquatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SquatView_Previews: PreviewProvider {
+    static var previews: some View {
+        SquatView()
+    }
+}
