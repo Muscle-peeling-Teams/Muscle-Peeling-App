8a12303397a1de3cf5277482a29cf4e2b3fb976f
ushiwaka7520 <21cm0117@jec.ac.jp>
2022-08-25 15:00:18 +0900

    test

--- Muscle-Peeling-App/ContentView.swift
+++ Muscle-Peeling-App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("風都探偵を見ろ")
+            Text("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
         }
     }
 }
