ef37a349eba660dfef5ad93d2e8c8f255f1ba9cd
45Watanabe <94812163+45Watanabe@users.noreply.github.com>
2022-08-26 10:26:02 +0900

    画像素材の追加、Viewの仮置き

--- Muscle-Peeling-App/SquatModel.swift
+++ Muscle-Peeling-App/SquatModel.swift
@@ -5,4 +5,38 @@
 //  Created by 渡辺幹 on 2022/08/24.
 //
 
-import Foundation
+import SwiftUI
+
+class SquatModel: ObservableObject {
+    @Published var numInSet = 10
+    @Published var setCount = 3
+    
+    var standValue = 0.0
+    var loweredValue = 0.0
+    
+    // 下がり幅の登録
+    // 立たせる -> 腰を落とす -> 戻す
+    func valueSetting() {
+        
+    }
+    
+    // 腰を落とす
+    func sitDown() {
+        
+    }
+    
+    // 腰を上げる
+    func standUp() {
+        
+    }
+    
+    // クリアチェッカー
+    func checker() {
+        
+    }
+    
+    // セットカウンター
+    func couter() {
+        
+    }
+}
--- Muscle-Peeling-App/SquatView.swift
+++ Muscle-Peeling-App/SquatView.swift
@@ -7,10 +7,69 @@
 
 import SwiftUI
 
+struct phone {
+    static let w = UIScreen.main.bounds.width
+    static let h = UIScreen.main.bounds.height
+}
+
 struct SquatView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+}
+
+struct InstructionTextView: View {
+    @State var text: String
+    var body: some View {
+        Text(text)
+    }
+}
+
+struct Reserve_SquatView: View {
+    var body: some View {
+        VStack {
+            Image("squat_stand")
+            InstructionTextView(text: InstructionTexts.squat_stand)
+        }
+    }
+}
+
+struct Stand_SquatView: View {
+    var body: some View {
+        VStack {
+            Image("squat_stand")
+            InstructionTextView(text: InstructionTexts.squat_stand)
+        }
+    }
+}
+
+struct Up_SquatView: View {
+    var body: some View {
+        Image("squat_stand")
+        InstructionTextView(text: InstructionTexts.squat_up)
+    }
+}
+
+struct Down_SquatView: View {
+    var body: some View {
+        Image("squat_sit")
+        InstructionTextView(text: InstructionTexts.squat_down)
+    }
+}
+
+struct Finish_SquatView: View {
+    var body: some View {
+        Image("squat_stand")
+        InstructionTextView(text: InstructionTexts.squat_finish)
+    }
+}
+
+struct InstructionTexts {
+    static let squat_reserve = "1セットあたりの回数と、行うセット数を選んでください。"
+    static let squat_stand = "スマホを画面を下に向けて持ち、足を肩幅に開いて腕を真っ直ぐに伸ばしましょう！"
+    static let squat_up = "腕を曲げぬよう、ゆっくりと腰を上げ、足を伸ばしましょう！"
+    static let squat_down = "腕を曲げぬよう、ゆっくりと腰を落としましょう！"
+    static let squat_finish = "お疲れさまでした！"
 }
 
 struct SquatView_Previews: PreviewProvider {
