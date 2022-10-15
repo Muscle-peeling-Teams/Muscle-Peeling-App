999112c5838c137b994af5e78e91274311a862a6
21cm0117清田 義常 <21cm0117@jec.ac.jp>
2022-10-15 13:55:05 +0900

    no message

--- Muscle-Peeling-App/Pushup/PushUpView.swift
+++ Muscle-Peeling-App/Pushup/PushUpView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct PushUpView: View {
     @ObservedObject var motionManager : PushUpMotionManager = .shared
     @State var shiftMenu = false
+    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         
@@ -21,57 +22,74 @@ struct PushUpView: View {
                 Text("第\(motionManager.trainingSetCount)セット 残り\(Int((motionManager.settingCount) - (motionManager.trainingCount)))回")
                     .font(.largeTitle)
                 
-                Image("plank")
+                Image("腕立て")
                     .resizable()
                     .frame(width: 300, height: 300)
                 
-                HStack(spacing: 30) {
-                    VStack {
-                        Spacer()
-                        Text("一時停止")
-                        Button(action: {
-                            if motionManager.trainingFinished == false{
-                                motionManager.pauseTraining()
-                            }else{
-                                //終了時には反応させない
+                if motionManager.trainingFinished == false{
+                    
+                    HStack(spacing: 30) {
+                        VStack {
+                            Spacer()
+                            Text("一時停止")
+                            Button(action: {
+                                if motionManager.trainingFinished == false{
+                                    motionManager.pauseTraining()
+                                }else{
+                                    //終了時には反応させない
+                                }
+                            }) {
+                                Text("| |")
+                                    .font(.largeTitle)
+                                    .bold()
+                                    .padding()
+                                    .frame(width: 100, height: 100)
+                                
+                                    .foregroundColor(Color.black)
+                                    .overlay(
+                                        Circle()
+                                            .stroke(Color.yellow, lineWidth: 3)
+                                    )
                             }
-                        }) {
-                            Text("| |")
-                                .font(.largeTitle)
-                                .bold()
-                                .padding()
-                                .frame(width: 100, height: 100)
-                            
-                                .foregroundColor(Color.black)
-                                .overlay(
-                                    Circle()
-                                        .stroke(Color.yellow, lineWidth: 3)
-                                )
+                            Spacer()
                         }
-                        Spacer()
-                    }
-                    VStack {
-                        Text("中止")
-                        Button(action: {
-                            motionManager.stopTraining()
-                            shiftMenu.toggle()
-                        }) {
-                            Text("■")
-                                .font(.largeTitle)
-                                .bold()
-                                .padding()
-                                .frame(width: 100, height: 100)
-                            
-                                .foregroundColor(Color.black)
-                                .overlay(
-                                    Circle()
-                                        .stroke(Color.red, lineWidth: 3)
-                                )
-                        }
-                        .fullScreenCover(isPresented: $shiftMenu) {
-                            MenuView()
+                        VStack {
+                            Text("中止")
+                            Button(action: {
+                                motionManager.stopTraining()
+                            }) {
+                                Text("■")
+                                    .font(.largeTitle)
+                                    .bold()
+                                    .padding()
+                                    .frame(width: 100, height: 100)
+                                
+                                    .foregroundColor(Color.black)
+                                    .overlay(
+                                        Circle()
+                                            .stroke(Color.red, lineWidth: 3)
+                                    )
+                            }
                         }
                     }
+                }else{
+                    Button {
+                        dismiss()
+                        dismiss()
+                    } label: {
+                        // 楕円形の描画
+                        Text("お疲れさまです")
+                            .font(.largeTitle)
+                            .frame(width: 360, height: 100)
+                            .foregroundColor(Color(.black))
+                            .background(Color(.white))
+                            .cornerRadius(24)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 1000)
+                                    .stroke(Color(.black), lineWidth: 1.0)
+                            )
+                    }
+                    Spacer()
                 }
             }
         }
